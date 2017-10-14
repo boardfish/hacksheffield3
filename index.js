355b9091c5bee9dcbd1c243f14ad85be55b08f76
JackHartley <jackhartley07@hotmail.co.uk>
2017-10-14 22:01:30 +0100

    Turned github.js into callable function

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var http = require('http');
-
+var gh = require('./github');
+gh.github_commits(github_callback);
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Hello Http');
+
 });
+
+function github_callback(data) {
+    console.log(data);
+}
 server.listen(8080);
