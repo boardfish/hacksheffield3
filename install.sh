32062f76099b39b376a916f412c8447e6f43c1c2
Simon Fish <si@mon.fish>
2017-10-15 06:44:21 +0100

    Working directory cleanup

--- install.sh
+++
@@ -1,9 +0,0 @@
-# npm update
-# npm install
-echo "Enter your GitHub personal access token for the GraphQL API:"
-read GITHUB_TOKEN
-echo "export GITHUB_TOOKEN=$GITHUB_TOKEN" >> ./testbingus
-echo "Enter your Trello developer key for its API:"
-read TRELLO_KEY
-echo "Enter your Trello developer token for its API:"
-read TRELLO_TOKEN
